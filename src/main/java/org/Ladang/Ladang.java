2a01d9c7dd497c3ce610551d84df49dec64119f4
FedrianzD <fedrianzd@gmail.com>
2024-05-09 15:30:37 +0700

    Create Ladang.java

---
+++ src/main/java/org/Ladang/Ladang.java
@@ -0,0 +1,23 @@
+package org.Ladang;
+
+public class Ladang {
+    public static final int Width_Default = 5;
+    public static final int Height_Default = 4;
+    private int Width;
+    private int Height;
+    private List<list<Kartu>>;
+    Ladang(int Width, int Height){
+
+    }
+    public void makeBigger(){
+
+    }
+    public void makeSmaller(){
+
+    }
+    public Boolean isCoordinateValid(int X, int Y){
+        return true;
+    }
+    public Boolean validateCardPlacement()
+
+}
