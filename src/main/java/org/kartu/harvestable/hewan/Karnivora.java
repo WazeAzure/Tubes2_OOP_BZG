9393dac7590c04da4d09f0d703b39b8d2a627ac9
AureliusJustin <88382259+AureliusJustin@users.noreply.github.com>
2024-05-10 16:31:11 +0700

    fix: Harvestable

--- src/main/java/org/kartu/harvestable/hewan/Karnivora.java
+++ src/main/java/org/kartu/harvestable/hewan/Karnivora.java
@@ -1,12 +1,20 @@
 package org.kartu.harvestable.hewan;
 
+import org.kartu.Kartu;
+import org.kartu.product.Product;
+
 public class Karnivora extends Hewan {
-    public Karnivora(String nama, String kategori, String imageURL, Integer valuePanen, String product) {
-        super(nama, kategori, imageURL, valuePanen, product);
+    public Karnivora(String nama, String kategori, String imageURL, Integer valuePanen) {
+        super(nama, kategori, imageURL, valuePanen);
     }
 
-    @Override
-    public void panen() {
-
+    public void makan(Kartu kartu) throws Exception {
+        if (kartu.getKategori().equals("Produk Hewan") || kartu.getKategori().equals("Produk Tumbuhan")) {
+            Product p = (Product) kartu;
+            value += p.getBerat();
+            valueEfek += p.getBerat();
+        } else {
+            throw new Exception("Kartu itu tidak bisa dimaakn!");
+        }
     }
 }
--- src/main/java/org/kartu/harvestable/hewan/Omnivora.java
+++ src/main/java/org/kartu/harvestable/hewan/Omnivora.java
@@ -1,12 +1,20 @@
 package org.kartu.harvestable.hewan;
 
+import org.kartu.Kartu;
+import org.kartu.product.Product;
+
 public class Omnivora extends Hewan {
-    public Omnivora(String nama, String kategori, String imageURL, Integer valuePanen, String product) {
-        super(nama, kategori, imageURL, valuePanen, product);
+    public Omnivora(String nama, String kategori, String imageURL, Integer valuePanen) {
+        super(nama, kategori, imageURL, valuePanen);
     }
 
-    @Override
-    public void panen() {
-
+    public void makan(Kartu kartu) throws Exception {
+        if (kartu.getKategori().equals("Produk Hewan") || kartu.getKategori().equals("Produk Tumbuhan")) {
+            Product p = (Product) kartu;
+            value += p.getBerat();
+            valueEfek += p.getBerat();
+        } else {
+            throw new Exception("Kartu itu tidak bisa dimaakn!");
+        }
     }
 }
