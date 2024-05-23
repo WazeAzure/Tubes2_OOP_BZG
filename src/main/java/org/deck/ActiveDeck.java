389af14bac88e1568c7daac8a37d02bb2bc73389
FedrianzD <fedrianzd@gmail.com>
2024-05-23 20:23:18 +0700

    fix: deck and toko

--- src/main/java/org/deck/ActiveDeck.java
+++ src/main/java/org/deck/ActiveDeck.java
@@ -3,15 +3,21 @@ package org.deck;
 import org.kartu.Kartu;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ActiveDeck{
     private Kartu[] listKartu = new Kartu[6];
     public ActiveDeck(){
-
+        Arrays.fill(listKartu, null);
     }
-    public void addCard(Kartu card, int index){
-        listKartu[index] = card;
+    public void addCard(List<Kartu> card){
+        int j=0;
+        for(int i = 0; i < listKartu.length; i++){
+            if(listKartu[i] == null){
+                listKartu[i] = card.get(j++);
+            }
+        }
     }
     public void removeCard(Kartu card, int index){
         listKartu[index] = null;
